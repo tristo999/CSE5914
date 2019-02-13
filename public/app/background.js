97a7020a69275e0c06b306ab9ba092bbdb7c8d65
tristo999 <tristanfitzwater@gmail.com>
2019-02-13 05:19:42 -0500

    Updated Content to support spotify

--- public/app/background.js
+++ public/app/background.js
@@ -20,22 +20,14 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
 			 "&response_type=token", 
 	  'interactive': true,  
 	},
-<<<<<<< HEAD
 	(redirect_url) => {	
 		console.log(redirect_url);
 		var url_string = redirect_url.replace("#","?");
-		console.log(url_string);
 		var url = new URL(url_string);
 		var c = url.searchParams.get("access_token");
-		console.log(c);
 		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 			chrome.tabs.sendMessage(tabs[0].id, {action: "access_token", token: c}, function(response) {});
 			});
-=======
-	function(redirect_url) {
-        console.log(redirect_url);
-        vars = getUrlVars(
->>>>>>> origin/feature/GoogleIdentify
 	});
 });		
 
